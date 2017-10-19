6c29dd235b4c1edc0601e5b18bfcf9e85fc4be4e
MrCsabaToth <csaba.toth.us@gmail.com>
2017-10-18 23:42:18 -0700

    Have users function survive anonymous users

--- functions/users.js
+++ functions/users.js
@@ -5,10 +5,18 @@ const admin = require('firebase-admin');
 
 exports.saveUserData = functions.auth.user().onCreate(({ data }) => {
   const uid = data.uid || data.providerData[0].uid;
+  const email = data.email || '';
+  const displayName = data.displayName || '';
+  const photoURL = data.photoURL || '';
+  if (!email && data.providerData) {
+    email = data.providerData[0].email;
+    displayName = data.providerData[0].displayName;
+    photoURL = data.providerData[0].photoURL;
+  }
   const userData = {
-    email: data.email || data.providerData[0].email || '',
-    displayName: data.displayName || data.providerData[0].displayName || '',
-    photoURL: data.photoURL || data.providerData[0].photoURL || ''
+    email: email,
+    displayName: displayName,
+    photoURL: photoURL
   };
 
   return admin.database().ref(`/users/${uid}`).set(userData);
